0a0e989733bdf846a501154ba47745fa0231d476
xmq12315 <30655484+xmq12315@users.noreply.github.com>
2018-01-05 10:06:47 +0800

    Default

--- xq_demo.py
+++ xq_demo.py
@@ -1,88 +1,98 @@
-from flask import Flask,request,render_template,url_for,session,redirect
+from flask import Flask, request, render_template, url_for, session, redirect
 from exts import db
-from models import User ,Question
+from models import User, Question
 import config
 
 app = Flask(__name__)
 app.config.from_object(config)
 db.init_app(app)
-method=['GET', 'POST']
+method = ['GET', 'POST']
 
-@app.route('/',methods = method)
-@app.route('/index/',methods = method)
+
+@app.route('/', methods=method)
+@app.route('/index/', methods=method)
 def hello_world():
-    qt=Question.query.all()
-    return render_template('index.html',content=qt)
+    qt = Question.query.all()
+    return render_template('index.html', content=qt)
 
-@app.route('/login/',methods=method)
+
+@app.route('/login/', methods=method)
 def login():
-    if request.method== 'GET':
+    if request.method == 'GET':
         return render_template('login.html')
     else:
-        telephone= request.form.get('telephone')
-        password= request.form.get('password')
-        user=User.query.filter(User.telephone == telephone and User.password==password).first()
+        telephone = request.form.get('telephone')
+        password = request.form.get('password')
+        user = User.query.filter(User.telephone == telephone and User.password == password).first()
         if user:
-            session['username']=user.name
+            session['username'] = user.name
             return redirect(url_for('hello_world'))
         else:
             return u'用户名或密码错误'
+
 
 @app.route('/logout/')
 def logout():
     session.pop('username')
     return redirect(url_for('login'))
 
-@app.route('/quest/' , methods=method)
+
+@app.route('/quest/', methods=method)
 def quest():
-    see=session.get('username')
+    see = session.get('username')
     if not see:
         return redirect(url_for('login'))
     else:
-        if request.method=='GET':
+        if request.method == 'GET':
             return render_template('question.html')
         else:
-            title=request.form.get('q-title')
-            content=request.form.get('q-content')
-            name_txt=session.get('username')
-            userid=User.query.filter(User.name==name_txt).first().id
-            question=Question(title=title,content=content,author_id=userid)
+            title = request.form.get('q-title')
+            content = request.form.get('q-content')
+            name_txt = session.get('username')
+            userid = User.query.filter(User.name == name_txt).first().id
+            question = Question(title=title, content=content, author_id=userid)
             db.session.add(question)
             db.session.commit()
             return redirect(url_for('hello_world'))
-@app.route('/reg/',methods=method)
+
+
+@app.route('/reg/', methods=method)
 def reg():
     if request.method == 'GET':
         print('get')
         return render_template('reg.html')
     else:
-        name=request.form.get('username')
-        telephone=request.form.get('telephone')
-        password1=request.form.get('password1')
-        password2=request.form.get('password2')
-        userid=User.query.filter(User.telephone==telephone).first()
+        name = request.form.get('username')
+        telephone = request.form.get('telephone')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        userid = User.query.filter(User.telephone == telephone).first()
         if userid:
             return u'手机号已被注册，请更换手机号'
         else:
             if password1 == password2:
-                user=User(name=name, telephone=telephone, password=password1)
+                user = User(name=name, telephone=telephone, password=password1)
                 db.session.add(user)
                 db.session.commit()
                 return redirect(url_for('hello_world'))
             else:
                 return u'密码不一致请重新输入'
-@app.route('/content/<list_id>',methods=method)
+
+
+@app.route('/content/<list_id>', methods=method)
 def content(list_id):
     if request.method == 'GET':
-        question=Question.query.filter(Question.id==list_id).first()
+        question = Question.query.filter(Question.id == list_id).first()
         print(question.author.name)
-        return render_template('content.html',qt=question)
+        return render_template('content.html', qt=question)
 
 
 @app.context_processor
 def username():
-    username=session.get('username')
-    username={'username':username}
+    username = session.get('username')
+    username = {'username': username}
     return username
+
+
 if __name__ == '__main__':
     app.run(debug=True)
